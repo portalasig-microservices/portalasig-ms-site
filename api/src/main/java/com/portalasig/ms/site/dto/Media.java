b07b7d7f0dcba5a8b3c8fe01cdffc486001b974a
fuhranku <frankponte95@gmail.com>
2025-04-20 22:29:30 -0400

    [TEG-96] add swagger to media dto

--- api/src/main/java/com/portalasig/ms/site/dto/Media.java
+++ api/src/main/java/com/portalasig/ms/site/dto/Media.java
@@ -2,6 +2,7 @@ package com.portalasig.ms.site.dto;
 
 import com.portalasig.ms.site.constant.MediaType;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +15,25 @@ import lombok.NoArgsConstructor;
 @ApiModel(description = "A media entity. Can be anything from pictures, videos, urls, audio, etc")
 public class Media {
 
+    @ApiModelProperty(value = "Media identifier")
     private Integer mediaId;
 
+    @ApiModelProperty(value = "Media name")
     private String name;
 
+    @ApiModelProperty(value = "Media description")
     private String description;
 
+    @ApiModelProperty(value = "Media url")
     private String url;
 
+    @ApiModelProperty(value = "Media filename (if applies)")
     private String fileName;
 
+    @ApiModelProperty(value = "Media file size (if applies)")
     private Long fileSize;
 
+    @ApiModelProperty(value = "Media Type")
     private MediaType mediaType;
 
 }
