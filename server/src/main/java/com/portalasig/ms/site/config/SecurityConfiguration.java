8ee92352229aa41d21e5f7efae6a5ecd7a2760fe
Frank Ponte <frankponte95@gmail.com>
2026-05-11 21:33:50 -0400

    wip: preserve current working state after docker master stabilization

--- server/src/main/java/com/portalasig/ms/site/config/SecurityConfiguration.java
+++ server/src/main/java/com/portalasig/ms/site/config/SecurityConfiguration.java
@@ -5,18 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * Configures security settings for the application, including OAuth2 JWT authentication
- * and default form login behavior.
+ /**
+ * Security configuration for the Site microservice.
  */
 @Configuration
 public class SecurityConfiguration {
 
-    @Value("${portalasig.security.oauth2.authorization-server.jwt.issuer-uri}")
-    private String jwtIssuerUri;
+    @Value("${portalasig.security.jwk-set-uri}")
+    private String internalJwksUri;
 
     @Bean
     SecurityFilterChain clientSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -24,9 +23,11 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated()
         );
-        http.oauth2ResourceServer(oauth -> oauth.jwt(
-                jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation(jwtIssuerUri)
-                )));
+        
+        http.oauth2ResourceServer(oauth -> oauth.jwt(jwt ->
+                jwt.decoder(NimbusJwtDecoder.withJwkSetUri(internalJwksUri).build())
+        ));
+        
         return http.build();
     }
 }
